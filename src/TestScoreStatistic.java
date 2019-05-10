80bd2946985c9382394a69ff448f378a7bef228c
jverea <jvereagovan2018@gmail.com>
2019-05-10 14:04:39 -0500

    update 5/10/19 java Block 3 2:04

--- src/TestScoreStatistic.java
+++ src/TestScoreStatistic.java
@@ -1,7 +1,17 @@
 public class TestScoreStatistic {
 
     public static void main (String [] args){
+        int high = 0;
+        int low = 100;
+        int ScoreEnterd;
+        System.out.println("Enter test score");
 
+        if (ScoreEnterd > high){
+            high = ScoreEnterd;
+        }
+        else if (ScoreEnterd < low){
+            low = ScoreEnterd;
 
+        }
     }
 }
