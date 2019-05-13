5cf683cb25ad31a820e57e8b632ef9a0c93a71e9
jverea <jvereagovan2018@gmail.com>
2019-05-13 13:18:02 -0500

    Update 5/13/19 Block 3 Java

--- src/TestScoreStatistic.java
+++ src/TestScoreStatistic.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 public class TestScoreStatistic {
 
     public static void main (String [] args){
         int high = 0;
         int low = 100;
-        int ScoreEnterd;
-        System.out.println("Enter test score");
+        int ScoreEnterd = 0;
+        int average;
+        int count = 0;
+        int totalScore = 0;
+        Scanner input = new Scanner(System.in);
 
-        if (ScoreEnterd > high){
-            high = ScoreEnterd;
-        }
-        else if (ScoreEnterd < low){
-            low = ScoreEnterd;
+        while(ScoreEnterd != 999) {
+            System.out.println("Enter students test score to quit type 999");
+            ScoreEnterd = input.nextInt();
 
+            if (ScoreEnterd > high) {
+                high = ScoreEnterd;
+            } else if (ScoreEnterd < low) {
+                low = ScoreEnterd;
+            }
+            count++;
+            totalScore = totalScore + ScoreEnterd;
         }
+        average = totalScore /count;
+        System.out.println("Here is the average of the test scores." + average);
     }
 }
